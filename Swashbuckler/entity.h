7ec2ad7be920ccb39b312a9a3bdea520e307187f
Xavier Bravo Guillen <xavibravo.guillen@gmail.com>
2017-10-07 12:19:42 +0200

    Entity/World structure created with basic definition

---
+++ Swashbuckler/entity.h
@@ -0,0 +1,10 @@
+#ifndef CLASS_ENTITY
+#define CLASS_ENTITY
+
+class Entity {
+
+
+
+};
+
+#endif // !CLASS_ENTITY
---
+++ Swashbuckler/world.h
@@ -0,0 +1,10 @@
+#ifndef CLASS_WORLD
+#define CLASS_WORLD
+
+class World {
+
+
+
+};
+
+#endif // !CLASS_WORLD
