e29736b21868ec7f12ac9941955451b5b94a5a3f
Xavier Bravo Guillen <xavibravo.guillen@gmail.com>
2017-10-07 12:08:34 +0200

    Adding basic user input loop

--- Swashbuckler/main.cpp
+++ Swashbuckler/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <stdio.h>
 #include <conio.h>
+#include <string>
 
 using namespace std;
 
 int main() {
 
-	cout << "Yarr world!" << endl << "Welcome to Swashbuckler!";
+	string userInput;
+	cout << "Yarr world!" << endl << "Welcome to Swashbuckler!" << endl;
+
+	while (1) 
+	{
+		getline(cin, userInput);
+		if (userInput == "Exit") 
+		{
+			break;
+		}
+	}
+
+	cout << "Thanks for playing Swashbuckler!" << endl << "See ya soon, landlover!" << endl;
 	_getch();
+
 	return 0;
 }
