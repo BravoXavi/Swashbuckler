7b29dfdc61a9d36524c8f14de490986caa687de5
Xavier Bravo Guillen <xavibravo.guillen@gmail.com>
2017-10-07 13:37:29 +0200

    Added two generic rooms

--- Swashbuckler/world.cpp
+++ Swashbuckler/world.cpp
@@ -1,5 +1,10 @@
 #include "world.h"
+#include "room.h"
 
-World::World() {}
+World::World() 
+{
+	Room sleeping_quarters("Sleeping quarters","It's kinda dark, and everyone is sleeping. I should not wake anyone...");
+	Room main_deck("Main deck", "Blablabla maindeck");
+}
 
 World::~World() {}
