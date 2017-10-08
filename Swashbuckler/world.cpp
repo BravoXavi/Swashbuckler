7293a402c73f6f94c8be101af8663f16e9482796
Xavier Bravo Guillen <xavibravo.guillen@gmail.com>
2017-10-08 15:24:36 +0200

    Orders from users are categorized and understanded

--- Swashbuckler/world.cpp
+++ Swashbuckler/world.cpp
@@ -2,6 +2,7 @@
 #include "room.h"
 #include "player.h"
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,10 +18,58 @@ World::~World() {}
 
 void World::readInput(vector<string> userInput) 
 {
-	cout << "Parsing..." << endl;
+	switch ( userInput.size() )
+	{
+		case 1:
+			if (userInput[0] == "Look")
+			{
+				cout << "Looking at something" << endl << ">";
+			}
+			else if (userInput[0] == "Inventory")
+			{
+				cout << "Checking inventory" << endl << ">";
+			}
+			else
+			{
+				cout << "I didn't understand you, mate" << endl << ">";
+			}
+			break;
 
-	for (vector<string>::iterator it = userInput.begin(); it != userInput.end(); ++it)
-		cout << *it << endl;
+		case 2:
+			if (userInput[0] == "Go")
+			{
+				if (userInput[1] == "north") cout << "Going north" << endl << ">";
+				else if (userInput[1] == "south") cout << "Going south" << endl << ">";
+				else if (userInput[1] == "east") cout << "Going east" << endl << ">";
+				else if (userInput[1] == "west") cout << "Going west" << endl << ">";
+				else cout << "That's not a valid direction!" << endl << ">";				
+			}
+			else if (userInput[0] == "Pick")
+			{
+				cout << "Picking something" << endl << ">";
+			}
+			else if (userInput[0] == "Drop")
+			{
+				cout << "Dropping something" << endl << ">";
+			}
+			else
+			{
+				cout << "I didn't understand you, mate" << endl << ">";
+			}
 
-	cout << "I didn't understand you, mate" << endl << ">";
+			break;
+
+		case 4:
+			if (userInput[0] == "Put")
+			{
+				cout << "Putting something in/on something" << endl << ">";
+			}
+			else
+			{
+				cout << "I didn't understand you, mate" << endl << ">";
+			}
+
+			break;
+
+	}
 }
