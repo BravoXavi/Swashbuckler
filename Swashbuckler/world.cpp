61b4bf180b3aca8a9cf3eb28b44e48dbef92e987
BravoXavi <BravoXavi@users.noreply.github.com>
2017-10-13 13:26:32 +0200

    Added all rooms of the game and exits

--- Swashbuckler/world.cpp
+++ Swashbuckler/world.cpp
@@ -10,23 +10,51 @@ using namespace std;
 
 World::World() 
 {
+	//Room Creation
 	Room* sleeping_quarters = new Room("Sleeping quarters", "It's kinda dark, and everyone is sleeping. I should not wake anyone...");
-	Room* main_deck = new Room("Main deck", "Blablabla maindeck");
+	Room* main_deck = new Room("Main deck", "need description");
+	Room* storage_room = new Room("Storage room", "need description");
+	Room* poopdeck = new Room("Poopdeck", "need description");
+	Room* crows_nest = new Room("Crow's nest", "need description");
 
+	//Exit creation and storage
 	Exit* exit1 = new Exit(up, sleeping_quarters, main_deck);
-	Exit* exit2 = new Exit(down, main_deck, sleeping_quarters);
-
 	sleeping_quarters->exits.push_back(exit1);
+
+	Exit* exit2 = new Exit(down, main_deck, sleeping_quarters);	
 	main_deck->exits.push_back(exit2);
 
+	Exit* exit3 = new Exit(east, sleeping_quarters, storage_room);
+	sleeping_quarters->exits.push_back(exit3);
+
+	Exit* exit4 = new Exit(west, storage_room, sleeping_quarters);
+	storage_room->exits.push_back(exit4);
+
+	Exit* exit5 = new Exit(up, main_deck, crows_nest);
+	main_deck->exits.push_back(exit5);
+
+	Exit* exit6 = new Exit(down, crows_nest, main_deck);	
+	crows_nest->exits.push_back(exit6);
+
+	Exit* exit7 = new Exit(north, main_deck, poopdeck);
+	main_deck->exits.push_back(exit7);
+
+	Exit* exit8 = new Exit(south, poopdeck, main_deck);
+	poopdeck->exits.push_back(exit8);
+
+	//Player creation
 	mainguy = new Player("Slinger", "A young, untrained but clever pirate", sleeping_quarters);
 
+	//Item creation and storage
 	Item* sword = new Item("Cutlass", "A slightly old, ugly but effective weapon.", true);
-	Item* letter = new Item("Letter", "Long description...", true);
-
 	sleeping_quarters->containedEntities.push_back(sword);
-	sleeping_quarters->containedEntities.push_back(letter);
 
+	Item* letter = new Item("Letter", "Long description...", true);
+	sleeping_quarters->containedEntities.push_back(letter);
+	
+	
+
+	//Storage of all entities in World class container
 	worldEntities.push_back(sleeping_quarters);
 	worldEntities.push_back(main_deck);
 	worldEntities.push_back(mainguy);
