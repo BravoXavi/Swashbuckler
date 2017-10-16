87a5ae347fec790753b62b1291261d506b041330
Xavier Bravo Guillen <xavibravo.guillen@gmail.com>
2017-10-17 00:00:38 +0200

    The structure of the user input reading was slightly modified to perform in real time

--- Swashbuckler/main.cpp
+++ Swashbuckler/main.cpp
@@ -14,15 +14,18 @@ int main() {
 	string userInput;
 	World theShip;
 	char inputKey;
+	vector<string> tokens;
 
-	cout << "Welcome to Swashbuckler!" << endl;
-	cout << "---------------------------------------------" << endl;
-	cout << "You wake up in the ship, in the middle of the night, with a sound different than the usual snoring of the crew. Steps." << endl;
-	cout << "With a quick look, you see the Captain passing silently between everyone. You notice a small letter with the navy seal falling from his pocket. THE NAVY SEAL?!" << endl;
-	cout << "You wait until the captain goes back to his room and check the letter. Bad news. Very, very bad news." << endl;
-	cout << "A true pirate would fight the Captain to take command of the ship and save your crew. A true pirate." << endl;
-	cout << "LET'S GET OUT OF HERE!!" << endl;
-	cout << "---------------------------------------------" << endl << ">";
+	std::cout << "Welcome to Swashbuckler! - The true pirates Zork-like game" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << "You wake up in the ship, in the middle of the night, with a sound different than the usual snoring of the crew. Steps." << std::endl;
+	std::cout << "With a quick look, you see the Captain passing silently between everyone. You notice a small letter with the navy seal falling from his pocket. THE NAVY SEAL?!" << std::endl;
+	std::cout << "You wait until the captain goes back to his room and check the letter. Bad news. Very, very bad news." << std::endl;
+	std::cout << "A true pirate would fight the Captain to take command of the ship and save your crew. A true pirate." << std::endl;
+	std::cout << "YOU BETTER GET OUT OF HERE SOON, LAD!!" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl << ">";
+
+	theShip.worldTimer = clock();
 
 	while (1) 
 	{			
@@ -33,26 +36,29 @@ int main() {
 			if (inputKey == '\b')
 			{
 				if (userInput.size() > 0) userInput.pop_back();
-				cout << '\b';
-				cout << ' ';
-				cout << '\b';
+				std::cout << '\b';
+				std::cout << ' ';
+				std::cout << '\b';
 			}
 			else if (inputKey == '\r')
 			{
-				istringstream iss(userInput);
-				vector<string> tokens{ istream_iterator<string>{iss},
-					istream_iterator<string>{} };
-				
-				std::cout << endl;
-				cout << "---------------------------------------------" << endl;
-				theShip.readInput(tokens);
+				string buf;
+				stringstream ss(userInput);
+				while (ss >> buf)
+					tokens.push_back(buf);
+
 				userInput.clear();
 			}
 			else
 			{
 				userInput = userInput + inputKey;
-				cout << inputKey;
+				std::cout << inputKey;
 			}
+		}
+
+		if (theShip.worldTurn(tokens))
+		{
+			tokens.clear();
 		}
 
 		if (theShip.mainguy->escaped)
@@ -63,8 +69,8 @@ int main() {
 	}
 
 	std::cout << "---------------------------------------------" << std::endl;
-	cout << "Thanks for playing Swashbuckler!" << endl << "See ya soon, landlubber!" << endl;
-	cout << "Xavier (Zelleryon) Bravo" << endl;
+	std::cout << "Thanks for playing Swashbuckler!" << endl << "See ya soon, landlubber!" << std::endl;
+	std::cout << "Xavier (Zelleryon) Bravo" << std::endl;
 	_getch();
 
 	return 0;
