6421797e196db9fed70cfd3020f833049134a992
BravoXavi <BravoXavi@users.noreply.github.com>
2017-10-18 12:19:40 +0200

    Added mood enum and methods of the badguy

--- Swashbuckler/npc.h
+++ Swashbuckler/npc.h
@@ -1,13 +1,24 @@
 #ifndef CLASS_NPC
 #define CLASS_NPC
 
-#include "entity.h"
+#include "creature.h"
 
-class Npc : public Entity {
+enum mood {
+	CALM,
+	MAD,
+	VIOLENT
+};
+
+class Npc : public Creature {
 
 public:
-	Npc(const char* name, const char* description);
+	Npc(const char* name, const char* description, Room* location);
 	~Npc();
+
+	bool aware = false;
+
+	void checkShip();
+	void getMad();
 
 private:
 
