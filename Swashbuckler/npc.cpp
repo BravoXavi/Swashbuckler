89a6ee859d09e2d78236fbbcfec34aeeadfa8ebe
BravoXavi <BravoXavi@users.noreply.github.com>
2017-10-18 12:19:52 +0200

    Small first implementation of bad guy

--- Swashbuckler/npc.cpp
+++ Swashbuckler/npc.cpp
@@ -1,12 +1,20 @@
 #include "npc.h"
+#include "room.h"
+#include <iostream>
 
-Npc::Npc(const char* npcName, const char* npcDescription) : Entity(npcName, npcDescription)
+Npc::Npc(const char* npcName, const char* npcDescription, Room* loc) : Creature(npcName, npcDescription, loc)
 {
 	name = npcName;
 	description = npcDescription;
 	entityType = npc;
+	location = loc;
 }
 
 Npc::~Npc()
 {
+}
+
+void Npc::checkShip()
+{
+	std::cout << "CHECKING SHIP" << std::endl;
 }
